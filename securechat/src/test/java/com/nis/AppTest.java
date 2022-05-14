887cc16075330ad218baaac39e5967cedbc6b262
Carl Combrinck <carljcombrinck@gmail.com>
2022-05-14 19:57:28 +0200

    Add test for RSA encryption and decryption

--- securechat/src/test/java/com/nis/AppTest.java
+++ securechat/src/test/java/com/nis/AppTest.java
@@ -4,6 +4,15 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
 
 import org.junit.Test;
 
@@ -28,4 +37,23 @@ public class AppTest
         byte[] uncompressed = PGPUtilities.decompress(compressed);
         assertTrue((new String(uncompressed)).equals(TEST_STRING));
     }
+
+    @Test
+    public void testRSA() throws IOException, NoSuchAlgorithmException, InvalidKeyException, 
+    NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException
+    {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        KeyPair pair = generator.generateKeyPair();
+
+        byte[] raw = TEST_STRING.getBytes();
+        byte[] encrypted = PGPUtilities.encryptWithRSA(raw, pair.getPrivate());
+        byte[] decrypted = PGPUtilities.decryptWithRSA(encrypted, pair.getPublic());
+        assertTrue((new String(decrypted)).equals(TEST_STRING));
+
+        encrypted = PGPUtilities.encryptWithRSA(raw, pair.getPublic());
+        decrypted = PGPUtilities.decryptWithRSA(encrypted, pair.getPrivate());
+        assertTrue((new String(decrypted)).equals(TEST_STRING));
+    }
+
 }
