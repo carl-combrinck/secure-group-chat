7f4e07e96c8bb14b5508ad97c8a19b9df6d465c0
Carl Combrinck <carljcombrinck@gmail.com>
2022-05-14 19:49:54 +0200

    Add test for PGP compression

--- securechat/src/test/java/com/nis/AppTest.java
+++ securechat/src/test/java/com/nis/AppTest.java
@@ -1,20 +1,31 @@
 package com.nis;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 
 import org.junit.Test;
+
+import com.nis.shared.PGPUtilities;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    private final String TEST_STRING = "0123456789" +
+                            "abcdefghijklmnopqrstuvwxyz" +
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                            ".,/;\"'\\{}[]-_=+?!@#$%^&*()`~";
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testCompression() throws IOException
     {
-        assertTrue( true );
+        byte[] raw = TEST_STRING.getBytes();
+        byte[] compressed = PGPUtilities.compress(raw);
+        byte[] uncompressed = PGPUtilities.decompress(compressed);
+        assertTrue((new String(uncompressed)).equals(TEST_STRING));
     }
 }
