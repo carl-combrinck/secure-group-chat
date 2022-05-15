681f03672688b669b3ff7645de2b4bc8883a9927
jaroncohen <jdicohen@gmail.com>
2022-05-15 23:30:44 +0200

    Add constructor to CertificateAuthority.java - retrieves CA KeyPair from KeyStore
    Add generateSignedCertificate method to CertificateAuthority.java
    - generates new certificate for Client and gets it signed by CA

--- securechat/src/main/java/com/nis/CertificateAuthority.java
+++ securechat/src/main/java/com/nis/CertificateAuthority.java
@@ -1,10 +1,11 @@
 package com.nis;
 
-import com.nis.shared.KeyUtilities;
-
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.*;
 
+import com.nis.shared.PGPUtilities;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
@@ -15,16 +16,59 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.asn1.x500.X500Name;
 
 import java.math.BigInteger;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 import java.util.*;
 
+/**
+ * CertificateAuthority is a class that generates a self-signed root X.509 certificate
+ * stored in a PKCS12 KeyStore. Additionally, it provides functionality to generate and sign
+ * X.509 certificates for Clients with its private key.
+ *
+ * @author Jaron Cohen
+ * @author Carl Combrinck
+ * @author Bailey Green
+ * @version 1.0.0
+ */
 public class CertificateAuthority {
+
+    /**
+     * Class constants
+     */
     private static final String KEYSTORENAME = "nis_group_chat_PKCS12";
     private static final char[] KEYSTOREPASSWORD = "nis2020".toCharArray();
+    private KeyPair CAKeyPair;
 
+    /**
+     * Class constructor - retrieves CA KeyPair from KeyStore on disk and stores in memory
+     * @throws KeyStoreException
+     * @throws UnrecoverableEntryException
+     * @throws NoSuchAlgorithmException
+     */
+    public CertificateAuthority() throws KeyStoreException, UnrecoverableEntryException, NoSuchAlgorithmException {
+        KeyStore CAKeyStore = KeyStore.getInstance("pkcs12");
+
+        try (FileInputStream fis = new FileInputStream(KEYSTORENAME)) {
+            CAKeyStore.load(fis, KEYSTOREPASSWORD);
+        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+            throw new RuntimeException(e);
+        }
+
+        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(KEYSTOREPASSWORD);
+        KeyStore.PrivateKeyEntry CAentry = (KeyStore.PrivateKeyEntry) CAKeyStore.getEntry("CA_cert",protParam);
+        PublicKey CAPublic = CAentry.getCertificate().getPublicKey();
+        PrivateKey CAPrivate = CAentry.getPrivateKey();
+
+        this.CAKeyPair = new KeyPair(CAPublic,CAPrivate);
+    }
+
+    /**
+     * Creates new KeyStore, generates CA KeyPair and stores self-signed root X.509 certificate in new KeyStore on disk.
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-            KeyPair CAKeyPair = KeyUtilities.generateRSAKeys();
+            KeyPair CAKeyPair = PGPUtilities.generateRSAKeyPair();
             X509Certificate CArootCertificate = generateCertificate("CA", CAKeyPair.getPublic(), CAKeyPair.getPrivate());
             KeyStore CAKeyStore = KeyStore.getInstance("pkcs12");
             CAKeyStore.load(null,KEYSTOREPASSWORD);
@@ -38,6 +82,14 @@ public class CertificateAuthority {
             }
     }
 
+    /**
+     * Generates a signed X.509 certificate using SHA256WithRSAEncryption by means of the Bouncy Castle Library.
+     * @param subj          Subject of the certificate
+     * @param publicKey     Public key associated with certificate
+     * @param caPrivateKey  Private key of the Certificate Authority for signing
+     * @return X509Certificate
+     * @throws Exception
+     */
     public static X509Certificate generateCertificate(String subj, PublicKey publicKey, PrivateKey caPrivateKey) throws Exception{
 
         X500Name issuer = new X500Name("CN=CA, L=Cape Town, C=ZA ");
@@ -61,4 +113,16 @@ public class CertificateAuthority {
                 .setProvider(new BouncyCastleProvider())
                 .getCertificate(certificateHolder);
     }
+
+    /**
+     * Method to generate signed X.509 certificates for Clients
+     * @param subject       Subject of the certificate (Client's identifier)
+     * @param publicKey     Public key associated with certificate (Client's Public Key)
+     * @return X509Certificate
+     * @throws Exception
+     */
+    public X509Certificate generateSignedCertificate(String subject, PublicKey publicKey ) throws Exception {
+        return generateCertificate(subject,publicKey,CAKeyPair.getPrivate());
+    }
+
 }
