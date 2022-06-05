3c3d3c7f2ee9819eda456f3980a4614ef2b56632
jaroncohen <jdicohen@gmail.com>
2022-06-05 18:05:55 +0200

    Add reply field to CertificateMessage.java

--- secure-group-chat/app/src/main/java/com/securegroupchat/CertificateMessage.java
+++ secure-group-chat/app/src/main/java/com/securegroupchat/CertificateMessage.java
@@ -4,13 +4,16 @@ import java.security.cert.X509Certificate;
 
 public class CertificateMessage extends Message {
     private X509Certificate certificate;
+    private boolean reply;
 
-    public CertificateMessage(String sender, String receiver, X509Certificate certificate) {
+    public CertificateMessage(String sender, String receiver, X509Certificate certificate, boolean reply) {
         super(sender, receiver);
         this.certificate = certificate;
+        this.reply = reply;
     }
 
     public X509Certificate getCertificate() {
-        return certificate;
+        return this.certificate;
     }
+    public boolean getReply() {return this.reply;}
 }
